ce670bf98e0d80fe7153942a54a2e92782d71f0b
bluestar7589 <bluestar7589@yahoo.com>
2024-08-24 12:25:28 -0700

    Added Member class

---
+++ Models/Member.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eCommerce_MVC_.Models
+{
+    public class Member
+    {
+        /// <summary>
+        /// This is represent ID for member
+        /// </summary>
+        [Key]
+        public int MemberId { get; set; }
+
+        /// <summary>
+        /// This is represent Email for member
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// This is represent Password for member
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// This is represent UserName for member
+        /// </summary>
+        public string UserName { get; set; }
+
+
+        /// <summary>
+        /// This is represent PhoneNumber for member
+        /// </summary>
+        public string PhoneNumber { get; set; }
+    }
+}
